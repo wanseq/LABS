8b989ae35d05dd08b511af731bd9145c41b78ef3
wanseq <heydavidsw@gmail.com>
2026-09-24 15:35:25 +0300

    + edit lab22 (*_*)

--- lab21.cpp
+++ lab21.cpp
@@ -14,89 +14,88 @@ int main()
 
     do
     {
-    cout << "Оберіть дію:" << endl;
-    cout << "0 - вийти з програми" << endl;
-    cout << "1 - знайти n-й елемент ряду" << endl;
-    cout << "2 - знайти суму n перших елементів" << endl;
-    cout << "3 - знайти суму ряду з точністю eps" << endl;
+        cout << "Оберіть дію:" << endl;
+        cout << "0 - вийти з програми" << endl;
+        cout << "1 - знайти n-й елемент ряду" << endl;
+        cout << "2 - знайти суму n перших елементів" << endl;
+        cout << "3 - знайти суму ряду з точністю eps" << endl;
 
-    cin >> Pr;
+        cin >> Pr;
 
-        if (Pr == 0)
+        switch (Pr)
         {
-            break;
-        }
-
-        if (Pr >= 1 && Pr <= 3)
-        {
-            switch (Pr)
+            case 0:
             {
-                case 1:
-                {
+                return 0;
+            }
+
+            case 1:
+            {
                 cout << "Введіть номер елемента n: ";
                 cin >> n;
 
                 u = z;
 
-                    for (k = 0; k < n; k++)
-                    {
-                        u *= (-z * z) /
-                            ((2 * k + 2) * (2 * k + 3));
-                    }
-
-                    cout << "n-й елемент ряду = " << u << endl;
-
-                    break;
+                for (k = 0; k < n; k++)
+                {
+                    u *= (-z * z) /
+                        ((2 * k + 2) * (2 * k + 3));
                 }
 
-                case 2:
-                {
+                cout << "n-й елемент ряду = " << u << endl;
+
+                break;
+            }
+
+            case 2:
+            {
                 cout << "Введіть кількість елементів n: ";
                 cin >> n;
 
                 u = z;
                 S = 0;
 
-                    for (k = 0; k < n; k++)
-                    {
-                        S = S + u;
+                for (k = 0; k < n; k++)
+                {
+                    S = S + u;
 
-                        u = u * (-z * z) /
-                            ((2 * k + 2) * (2 * k + 3));
-                    }
-
-                    cout << "Сума перших " << n
-                         << " елементів = " << S << endl;
-
-                    break;
+                    u = u * (-z * z) /
+                        ((2 * k + 2) * (2 * k + 3));
                 }
 
-                case 3:
-                {
+                cout << "Сума перших " << n
+                     << " елементів = " << S << endl;
+
+                break;
+            }
+
+            case 3:
+            {
                 u = z;
                 S = 0;
                 k = 0;
 
-                    while (fabs(u) >= eps)
-                    {
-                        S = S + u;
+                while (fabs(u) >= eps)
+                {
+                    S = S + u;
 
-                        u = u * (-z * z) /
-                            ((2 * k + 2) * (2 * k + 3));
+                    u = u * (-z * z) /
+                        ((2 * k + 2) * (2 * k + 3));
 
-                        k++;
-                    }
-
-                    cout << "Сума ряду = " << S << endl;
-                    cout << "Перевірка sin(z) = " << sin(z) << endl;
-
-                    break;
+                    k++;
                 }
+
+                cout << "Сума ряду = " << S << endl;
+                cout << "Перевірка sin(z) = " << sin(z) << endl;
+
+                break;
             }
-        }
-        else
-        {
-            cout << "Неправильно введене значення Pr." << endl;
+
+            default:
+            {
+                cout << "Неправильно введене значення Pr." << endl;
+                break;
+            }
         }
 
         cout << endl;
